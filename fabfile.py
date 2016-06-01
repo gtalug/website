6df2deeb8e8a0ebb99e9dd88d9bc5ab24585ee27
Myles Braithwaite <me@mylesbraithwaite.com>
2016-06-01 10:24:01 -0400

    Removing the stupid deploy thingy.

--- fabfile.py
+++ fabfile.py
@@ -67,7 +67,6 @@ def deploy():
         extra_opts='--exclude=".DS_Store" --exclude="static/less/" \
  --exclude="static/.webassets-cache/" --exclude="static/js/less-1.5.0.min.js"'
     )
-    register_deployment(".")
 
 
 @task
@@ -98,26 +97,3 @@ def remote_build():
         # Build and deploy the website. 
         run('./env/bin/python website.py build')
         run('cp -r %s/* /srv/www/org_gtalug_www/html/' % env.build_path)
-
-
-def check_working_dir_clean():
-    """Aborts if not everything has been committed."""
-    # Inspiration:
-    # http://stackoverflow.com/questions/5139290/how-to-check-if-theres-nothing-to-be-committed-in-the-current-branch
-    with settings(warn_only=True):
-        if not local('git diff --stat --exit-code').succeeded:
-            abort('You have unstaged changes: to ignore, run with check_clean=no')
-        if not local('git diff --cached --stat --exit-code').succeeded:
-            abort('Your index contains uncommitted changes: to ignore, run with check_clean=no')
-
-        r = local('git ls-files --other --exclude-standard --directory', capture=True )
-        if r != '':
-            abort('Untracked files exist: to ignore, run with check_clean=no')
-
-
-@task
-@runs_once
-def register_deployment(git_path):
-    with hide('warnings'):
-        with(lcd(git_path)):
-            local("curl https://opbeat.com/api/v1/organizations/b33f729826be44e5b889ae0a8ec88eea/apps/049496b911/releases/ -H 'Authorization: Bearer 609dd34cebcf1f830036d102cf1be8d811c70a91' -d rev=`git log -n 1 --pretty=format:%H` -d branch=`git rev-parse --abbrev-ref HEAD` -d status=completed")
