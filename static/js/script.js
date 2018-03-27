1d0f8c6e898b04ef106ecefc5aac1a95b59d48bb
Alex Volkov <alex@flamy.ca>
2018-03-26 20:06:36 -0400

    Updated token.

--- static/js/script.js
+++ static/js/script.js
@@ -17,8 +17,8 @@ var mapIcon = L.icon({
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
-    id: 'myles.mp1f6ado',
-    accessToken: 'pk.eyJ1IjoibXlsZXMiLCJhIjoiYzM4NjE3NjA4ZjYxYzJhMzJjNjU3MzcxZWEyYzRhMGIifQ.V_-nhDa5NoL9uhK7OqUOew'
+    id: 'gtalug-home',
+    accessToken: 'pk.eyJ1IjoiZ3RhbHVnIiwiYSI6ImNqZWVramt4dzFkMXcyeGxhanltdGg5dXEifQ.eoBUNwjc5f5nPPnmDmlYzw'
 }).addTo(map);
 
 var marker = L.marker([43.6577,-79.3773], {icon: mapIcon}).addTo(map);
